3735b881fb4359ecad0669acfa103f7535c1a59e
WhatsARanjit <whatsaranjit@gmail.com>
2018-07-07 22:55:00 -0400

    Prettying up webpage with a template

--- docker/app.rb
+++ docker/app.rb
@@ -5,29 +5,18 @@ set :bind, '0.0.0.0'
 set :port, ENV['port']
 
 get '/web' do
-  dbhost = ENV['dbhost'] || 'localhost'
-  dbport = ENV['dbport'] || 8081
-  dbpass = ENV['dbpass'] || 'password123'
-  uri    = URI(%(http://#{dbhost}:#{dbport}/db?password=#{dbpass}))
+  @dbhost = ENV['dbhost'] || 'localhost'
+  dbport  = ENV['dbport'] || 8081
+  dbpass  = ENV['dbpass'] || 'password123'
+  uri     = URI(%(http://#{@dbhost}:#{dbport}/db?password=#{dbpass}))
   begin
-    dbname = Net::HTTP.get(uri)
+    @dbname = Net::HTTP.get(uri)
   rescue StandardError
-    dbname = 'unreachable'
+    @dbname = 'unreachable'
   end
-  res = [
-    '<h2>',
-    %(Web machine: #{Socket.gethostname}),
-    dbname,
-    '</h2>'
-  ]
-  res.join('<br />')
+  erb :web
 end
 
 get '/db' do
-  res = [
-    '<h3>',
-    %(DB machine: #{Socket.gethostname}),
-    '</h3>'
-  ]
-  res.join('<br />')
+  Socket.gethostname
 end
